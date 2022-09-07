46618d2febae9f73aeab1457fa805551ddea716f
shreyamadiga <shreyamadiga@gmail.com>
2022-09-07 06:49:32 +0000

    first commit

---
+++ app/javascript/controllers/index.js
@@ -0,0 +1,25 @@
+// Import and register all your controllers from the importmap under controllers/*
+
+import { application } from "controllers/application"
+
+// Eager load all controllers defined in the import map under controllers/**/*_controller
+import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
+import StimulusReflex from 'stimulus_reflex'
+import consumer from '../channels/consumer'
+import controller from '../controllers/application_controller'
+eagerLoadControllersFrom("controllers", application)
+
+// Lazy load controllers as they appear in the DOM (remember not to preload controllers in import map!)
+// import { lazyLoadControllersFrom } from "@hotwired/stimulus-loading"
+// lazyLoadControllersFrom("controllers", application)
+StimulusReflex.initialize(application, { consumer, controller, isolate: true })
+StimulusReflex.debug = process.env.RAILS_ENV === 'development'
+
+// import Flatpickr
+import Flatpickr from 'stimulus-flatpickr'
+
+// Import style for flatpickr
+import "flatpickr/dist/themes/dark.css"
+
+// Manually register Flatpickr as a stimulus controller
+application.register('flatpickr', Flatpickr)
